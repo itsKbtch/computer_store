af7ef612fc2b97f555d68a17c09a8b69fd423738
itsKbtch <itsKbtch@users.noreply.github.com>
2019-07-09 10:41:56 +0700

    Update edit.blade.php

--- resources/views/admin/stock/edit.blade.php
+++ resources/views/admin/stock/edit.blade.php
@@ -32,7 +32,7 @@
             <!-- Page Header -->
             <div class="page-header row no-gutters py-4">
               <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
-                <span class="text-uppercase page-subtitle">Thêm mới</span>
+                <span class="text-uppercase page-subtitle">Chỉnh sửa</span>
                 <h3 class="page-title">Kho hàng</h3>
               </div>
             </div>
